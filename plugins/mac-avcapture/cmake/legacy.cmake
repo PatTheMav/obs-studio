5c77470edf792bc10b55c069c3b2771c7251de25
PatTheMav <PatTheMav@users.noreply.github.com>
2022-11-22 20:47:48 +0100

    mac-avcapture: Add changes for CMake build framework 3.0
    New code path only taken if OBS_CMAKE_VERSION is set to 3.0.0 or
    greater, old functionality remains unchanged.

--- plugins/mac-avcapture/cmake/legacy.cmake
+++ plugins/mac-avcapture/cmake/legacy.cmake
@@ -1,6 +1,4 @@
-cmake_minimum_required(VERSION 3.22...3.25)
-
-legacy_check()
+project(mac-avcapture)
 
 find_library(AVFOUNDATION AVFoundation)
 find_library(COCOA Cocoa)
@@ -8,16 +6,17 @@ find_library(COREFOUNDATION CoreFoundation)
 find_library(COREMEDIA CoreMedia)
 find_library(COREVIDEO CoreVideo)
 find_library(COREMEDIAIO CoreMediaIO)
+
 mark_as_advanced(AVFOUNDATION COCOA COREFOUNDATION COREMEDIA COREMEDIAIO COREVIDEO)
 
 add_library(mac-avcapture MODULE)
 add_library(OBS::avcapture ALIAS mac-avcapture)
 
 target_sources(mac-avcapture PRIVATE av-capture.mm left-right.hpp scope-guard.hpp)
+
 target_compile_features(mac-avcapture PRIVATE cxx_std_11)
-if(NOT XCODE)
-  set_source_files_properties(av-capture.mm PROPERTIES COMPILE_FLAGS -fobjc-arc)
-endif()
+
+set_source_files_properties(av-capture.mm PROPERTIES COMPILE_FLAGS -fobjc-arc)
 
 target_link_libraries(
   mac-avcapture
@@ -29,8 +28,6 @@ target_link_libraries(
           ${COREVIDEO}
           ${COREMEDIAIO})
 
-set_target_properties_obs(
-  mac-avcapture
-  PROPERTIES FOLDER plugins
-             PREFIX ""
-             XCODE_ATTRIBUTE_CLANG_ENABLE_OBJC_ARC YES)
+set_target_properties(mac-avcapture PROPERTIES FOLDER "plugins" PREFIX "")
+
+setup_plugin_target(mac-avcapture)
