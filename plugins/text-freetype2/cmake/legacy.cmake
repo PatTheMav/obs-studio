7a249d92ad51e570e1500e58d63db46845f0d2db
PatTheMav <PatTheMav@users.noreply.github.com>
2023-01-25 17:49:07 +0100

    text-freetype2: Add changes for CMake build framework 3.0
    New code path only taken if OBS_CMAKE_VERSION is set to 3.0.0 or
    greater, old functionality remains unchanged.

--- plugins/text-freetype2/cmake/legacy.cmake
+++ plugins/text-freetype2/cmake/legacy.cmake
@@ -1,11 +1,9 @@
-cmake_minimum_required(VERSION 3.22...3.25)
-
-legacy_check()
+project(text-freetype2)
 
 option(ENABLE_FREETYPE "Enable FreeType text plugin" ON)
 
 if(NOT ENABLE_FREETYPE)
-  target_disable(text-freetype2)
+  obs_status(DISABLED "text-freetype2")
   return()
 endif()
 
@@ -16,24 +14,33 @@ add_library(OBS::text-freetype2 ALIAS text-freetype2)
 
 target_sources(text-freetype2 PRIVATE find-font.h obs-convenience.c text-functionality.c
                                       text-freetype2.c obs-convenience.h text-freetype2.h)
+
 target_link_libraries(text-freetype2 PRIVATE OBS::libobs Freetype::Freetype)
 
+set_target_properties(text-freetype2 PROPERTIES FOLDER "plugins" PREFIX "")
+
 if(OS_WINDOWS)
-  configure_file(cmake/windows/obs-module.rc.in text-freetype2.rc)
+  set(MODULE_DESCRIPTION "OBS FreeType text module")
+  configure_file(${CMAKE_SOURCE_DIR}/cmake/bundle/windows/obs-module.rc.in text-freetype2.rc)
+
   target_sources(text-freetype2 PRIVATE find-font.c find-font-windows.c text-freetype2.rc)
-  target_link_options(text-freetype2 PRIVATE /IGNORE:4098 /IGNORE:4099)
+  target_link_options(text-freetype2 PRIVATE "LINKER:/IGNORE:4098" "LINKER:/IGNORE:4099")
+
 elseif(OS_MACOS)
   find_package(Iconv REQUIRED)
   find_library(COCOA Cocoa)
   mark_as_advanced(COCOA)
 
   target_sources(text-freetype2 PRIVATE find-font.c find-font-cocoa.m find-font-iconv.c)
+
   target_link_libraries(text-freetype2 PRIVATE Iconv::Iconv ${COCOA})
-elseif(OS_LINUX OR OS_FREEBSD)
+
+elseif(OS_POSIX)
   find_package(Fontconfig REQUIRED)
 
   target_sources(text-freetype2 PRIVATE find-font-unix.c)
+
   target_link_libraries(text-freetype2 PRIVATE Fontconfig::Fontconfig)
 endif()
 
-set_target_properties_obs(text-freetype2 PROPERTIES FOLDER plugins PREFIX "")
+setup_plugin_target(text-freetype2)
