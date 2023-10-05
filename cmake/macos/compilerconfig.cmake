b6afc6aa3207bc9b6522d2aec364c34c4a5b6ad2
PatTheMav <PatTheMav@users.noreply.github.com>
2023-10-05 18:31:19 +0200

    cmake: Remove support for generators besides Xcode on macOS

--- cmake/macos/compilerconfig.cmake
+++ cmake/macos/compilerconfig.cmake
@@ -2,6 +2,10 @@
 
 include_guard(GLOBAL)
 
+if(NOT XCODE)
+  message(FATAL_ERROR "Building OBS Studio on macOS requires Xcode generator.")
+endif()
+
 include(ccache)
 include(compiler_common)
 
@@ -15,63 +19,33 @@ if(NOT CMAKE_OSX_ARCHITECTURES)
 endif()
 set_property(CACHE CMAKE_OSX_ARCHITECTURES PROPERTY STRINGS arm64 x86_64)
 
-if(XCODE)
-  # Enable dSYM generator for release builds
-  string(APPEND CMAKE_C_FLAGS_RELEASE " -g")
-  string(APPEND CMAKE_CXX_FLAGS_RELEASE " -g")
-else()
-  option(ENABLE_COMPILER_TRACE "Enable clang time-trace (requires Ninja)" OFF)
-  mark_as_advanced(ENABLE_COMPILER_TRACE)
+# Enable dSYM generator for release builds
+string(APPEND CMAKE_C_FLAGS_RELEASE " -g")
+string(APPEND CMAKE_CXX_FLAGS_RELEASE " -g")
+string(APPEND CMAKE_OBJC_FLAGS_RELEASE " -g")
+string(APPEND CMAKE_OBJCXX_FLAGS_RELEASE " -g")
 
-  # clang options for ObjC
-  set(_obs_clang_objc_options
-      ${_obs_clang_common_options}
-      -Wno-implicit-atomic-properties
-      -Wno-objc-interface-ivars
-      -Warc-repeated-use-of-weak
-      -Wno-arc-maybe-repeated-use-of-weak
-      -Wimplicit-retain-self
-      -Wduplicate-method-match
-      -Wshadow
-      -Wfloat-conversion
-      -Wobjc-literal-conversion
-      -Wno-selector
-      -Wno-strict-selector-match
-      -Wundeclared-selector
-      -Wdeprecated-implementations
-      -Wprotocol
-      -Werror=block-capture-autoreleasing
-      -Wrange-loop-analysis)
+# Default ObjC compiler options used by Xcode:
+#
+# * -Wno-implicit-atomic-properties
+# * -Wno-objc-interface-ivars
+# * -Warc-repeated-use-of-weak
+# * -Wno-arc-maybe-repeated-use-of-weak
+# * -Wimplicit-retain-self
+# * -Wduplicate-method-match
+# * -Wshadow
+# * -Wfloat-conversion
+# * -Wobjc-literal-conversion
+# * -Wno-selector
+# * -Wno-strict-selector-match
+# * -Wundeclared-selector
+# * -Wdeprecated-implementations
+# * -Wprotocol
+# * -Werror=block-capture-autoreleasing
+# * -Wrange-loop-analysis
 
-  # clang options for ObjC++
-  set(_obs_clang_objcxx_options ${_obs_clang_objc_options} -Wno-non-virtual-dtor)
-
-  # cmake-format: off
-  add_compile_options(
-    "$<$<COMPILE_LANGUAGE:C>:${_obs_clang_c_options}>"
-    "$<$<COMPILE_LANGUAGE:CXX>:${_obs_clang_cxx_options}>"
-    "$<$<COMPILE_LANGUAGE:OBJC>:${_obs_clang_objc_options}>"
-    "$<$<COMPILE_LANGUAGE:OBJCXX>:${_obs_clang_objcxx_options}>")
-  # cmake-format: on
-
-  # Enable stripping of dead symbols when not building for Debug configuration
-  set(_release_configs RelWithDebInfo Release MinSizeRel)
-  if(CMAKE_BUILD_TYPE IN_LIST _release_configs)
-    add_link_options(LINKER:-dead_strip)
-  endif()
-
-  # Enable color diagnostics for AppleClang
-  set(CMAKE_COLOR_DIAGNOSTICS ON)
-
-  # Add time trace option to compiler, if enabled.
-  if(ENABLE_COMPILER_TRACE AND CMAKE_GENERATOR STREQUAL "Ninja")
-    add_compile_options($<$<NOT:$<COMPILE_LANGUAGE:Swift>>:-ftime-trace>)
-  else()
-    set(ENABLE_COMPILER_TRACE
-        OFF
-        CACHE BOOL "Enable clang time-trace (requires Ninja)" FORCE)
-  endif()
-endif()
+# Default ObjC++ compiler options used by Xcode: *  -Wno-non-virtual-dtor
 
 add_compile_definitions(
-  "$<$<NOT:$<COMPILE_LANGUAGE:Swift>>:$<$<CONFIG:DEBUG>:DEBUG>;$<$<CONFIG:DEBUG>:_DEBUG>;SIMDE_ENABLE_OPENMP>")
+  $<$<NOT:$<COMPILE_LANGUAGE:Swift>>:$<$<CONFIG:DEBUG>:DEBUG>>
+  $<$<NOT:$<COMPILE_LANGUAGE:Swift>>:$<$<CONFIG:DEBUG>:_DEBUG>> $<$<NOT:$<COMPILE_LANGUAGE:Swift>>:SIMDE_ENABLE_OPENMP>)
