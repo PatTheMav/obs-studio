6713d0cd5b5434472222824a6d39a1247ebc881e
PatTheMav <PatTheMav@users.noreply.github.com>
2026-06-30 21:35:33 +0200

    CI: Update generate-docs GitHub action
    The action is updated to follow new best practices for composite actions
    within the project:
    
    * Larger script snippets (line number > 5) are factored out into
      separate files.
    * Bash script is linted by ShellCheck.
    * Added action input to specify checkout directory.
    * Action uses runner temp to generate documentation.
    * Arguments use "dash-case" as is common for GitHub actions.

---
+++ .github/actions/generate-docs/setup-action.bash
@@ -0,0 +1,89 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2154,SC1091
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+: "${CI:?}"
+if [[ -n "${RUNNER_DEBUG}" ]]; then set -x; fi
+
+fixup-documentation() {
+  # This expression will first try to match all the LIBOBS_API_[...]_VER
+  # lines in 'obs-config.h', before removing the '#define ' prefix and
+  # trimming away whitespace characters and linebreaks.
+  # This will yield a single line with the each major, minor, and patch
+  # version variable name followed by its value, so every even item
+  # in this string will contain a version part.
+  local version_lines
+  version_lines="$(grep \
+    --extended-regexp \
+    --regexp="#define LIBOBS_API_(MAJOR|MINOR|PATCH)_VER *" "${checkout}/libobs/obs-config.h" \
+      | sed 's/#define //g' \
+      | tr -s ' ' \
+      | tr '\n' ' ')"
+
+  local major
+  local minor
+  local patch
+  read -r _ major _ minor _ patch _ <<< "${version_lines}"
+
+  local current_year
+  current_year="$(date +"%Y")"
+
+  # This expression simply replaces the definition of the 'version' and
+  # 'release' variables in the Python script with updated variants using
+  # the version tokens set by the previous expression.
+  # The copyright variable assignment is updated to use the current
+  # local year as the second year value.
+  sed -i -E \
+    -e "s/version = '([0-9]+\.[0-9]+\.[0-9]+)'/version = '${major}.${minor}.${patch}'/g" \
+    -e "s/release = '([0-9]+\.[0-9]+\.[0-9]+)'/release = '${major}.${minor}.${patch}'/g" \
+    -e "s/copyright = '(2017-[0-9]+, Lain Bailey)'/copyright = '2017-${current_year}, Lain Bailey'/g" \
+    "${checkout}/docs/sphinx/conf.py"
+
+  if [[ "${DISABLE_LINK_EXTENSIONS:-false}" == 'true' ]]; then
+    sed -i -e "s/html_link_suffix = None/html_link_suffix = ''/g" \
+     "${checkout}/docs/sphinx/conf.py"
+    echo "artifactName=OBS Studio Docs (No Extensions)" >> "${GITHUB_OUTPUT}"
+  else
+    echo "artifactName=OBS Studio Docs" >> "${GITHUB_OUTPUT}"
+  fi
+}
+
+setup-action() {
+  local checkout="${PWD}"
+  if ! [[ -d "${checkout}/.git" && -d "${checkout}/docs/sphinx" ]]; then
+    echo '::error::Action needs to be run from an obs-studio checkout root directory'
+    return 1
+  fi
+
+  local docs_dir="${checkout}/docs/sphinx"
+
+  echo '::group::Python Setup'
+
+  if [[ "${RUNNER_OS}" == 'Linux' ]]; then
+    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv || true)"
+    echo "/home/linuxbrew/.linuxbrew/bin:/home/linuxbrew/.linuxbrew/sbin" >> "${GITHUB_PATH}"
+  fi
+
+  brew install --quiet python3
+
+  python3 -m venv "${RUNNER_TEMP}/generate-docs-venv"
+  echo '::endgroup::'
+
+  echo '::group::Sphinx Setup'
+  source "${RUNNER_TEMP}/generate-docs-venv/bin/activate"
+  python3 -m pip install --upgrade sphinx poetry
+
+  python3 -m pip install --requirement "${docs_dir}/requirements.txt"
+  echo '::endgroup::'
+
+  echo "commitHash=${GITHUB_SHA:0:9}" >> "${GITHUB_OUTPUT}"
+
+  fixup-documentation
+
+  deactivate
+}
+
+setup-action
