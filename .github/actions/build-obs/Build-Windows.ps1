fc23109f3a642dd32c72f92f8dedc95bec89c29b
PatTheMav <PatTheMav@users.noreply.github.com>
2026-06-30 21:16:58 +0200

    CI: Update build-obs GitHub action
    The action is updated to follow new best practices for composite actions
    within the project:
    
    * Larger script snippets (line number > 5) are factored out into
      separate files.
    * Bash script is linted by ShellCheck.
    * Zsh script is an adaptation of the Bash script with macOS-specific
      additions.
    * Powershell script is linted by Invoke-ScriptAnalyzer.
    * Added action input to specify build directory, using the runner temp
      as default.
    * Arguments use "dash-case" as is common for GitHub actions.
    
    Also updates all workflows and actions that invoked the action to use
    the new arguments.

---
+++ .github/actions/build-obs/Build-Windows.ps1
@@ -0,0 +1,50 @@
+#Requires -Version 7.3
+
+[CmdletBinding(PositionalBinding=$false)]
+param()
+
+begin {
+  if ($null -eq $env:CI) { throw }
+  if ($null -ne $env:RUNNER_DEBUG) { Set-PSDebug -Trace 1 }
+  $ErrorActionPreference = 'Stop'
+
+  $Checkout = Get-Location | Get-Item
+
+  if (!((Test-Path -Path "${Checkout}/.git") -and (Test-Path -Path "${Checkout}/CMakePresets.json"))) {
+    Write-Output '::error::Action needs to be run from the root directory of an obs-studio checkout.'
+    throw
+  }
+
+  if (!(Test-Path -Path $env:OUTPUT_PATH)) {
+    New-Item -Type Directory -Path $env:OUTPUT_PATH > $null
+  }
+  $BuildLocation = $(
+    $PresetJson = Get-Content "${Checkout}/CMakePresets.json"
+    $PresetBuildLocation = ((($PresetJson | ConvertFrom-JSON).configurePresets) | Where-Object {
+      $_.name -eq "windows-${env:BUILD_TARGET}"
+    }).binaryDir
+    $PresetBuildLocation -replace '\$\{sourceDir\}',$env:OUTPUT_PATH
+  )
+}
+
+process {
+  Write-Output '::group::Configure obs-studio'
+  $CmakeArgs = @(
+    '--preset', "windows-ci-${env:BUILD_TARGET}"
+    '-B', $BuildLocation
+    $( if ($null -ne $env:RUNNER_DEBUG) {'--debug-output'} )
+  )
+  & cmake @CmakeArgs
+  Write-Output '::endgroup::'
+
+  Write-Output '::group::Build obs-studio'
+  $CmakeBuildArgs = @(
+    '--build', $BuildLocation
+    '--config', $env:BUILD_CONFIG
+    '--parallel'
+    $( if ($null -ne $env:RUNNER_DEBUG) {'--verbose'} )
+    '--', '/consoleLoggerParameters:Summary', '/nologo'
+  )
+  & cmake @CmakeBuildArgs
+  Write-Output '::endgroup::'
+}
