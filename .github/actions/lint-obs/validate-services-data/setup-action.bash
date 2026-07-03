77cb709ab44420b35d1ae3274f4634aab3e8d38e
PatTheMav <PatTheMav@users.noreply.github.com>
2026-07-03 15:42:05 +0200

    CI: Add validate-services-data action
    The action is updated to follow new best practices for composite actions
    within the project:
    
    * Larger script snippets (line number > 5) are factored out into
      separate files.
    * Bash script is linted by ShellCheck.
    * Update used arguments to use simpler words.
    * Arguments use "dash-case" as is common for GitHub actions.
    
    Also updates all workflows and actions that invoked the action to use
    the new arguments.

---
+++ .github/actions/lint-obs/validate-services-data/setup-action.bash
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2154,SC1091
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+: "${CI:?}"
+if [[ -n "${RUNNER_DEBUG:-}" ]]; then set -x; fi
+
+setup-action() {
+  echo '::group::Python Setup'
+  if [[ "${RUNNER_OS}" == 'Linux' ]]; then
+    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv || true)"
+    echo "/home/linuxbrew/.linuxbrew/bin:/home/linuxbrew/.linuxbrew/sbin" >> "${GITHUB_PATH}"
+  fi
+
+  brew install --quiet python3
+
+  python3 -m venv "${RUNNER_TEMP}/services-validator-venv"
+  echo '::endgroup::'
+
+  echo '::group::Install Action dependencies'
+  source "${RUNNER_TEMP}/services-validator-venv/bin/activate"
+  python3 -m pip install --upgrade jsonschema json_source_map requests aiohttp
+  echo '::endgroup::'
+
+  deactivate
+}
+
+setup-action
