a4a11c3dcdd7f479c8c4d1eb6ed8991fbc54dc6d
PatTheMav <PatTheMav@users.noreply.github.com>
2026-07-03 15:47:39 +0200

    C: Add setup-bouf action

--- .github/actions/codesign-obs/setup-windows/Setup-GCP.ps1
+++ .github/actions/codesign-obs/setup-windows/Setup-GCP.ps1
@@ -4,23 +4,17 @@
 param()
 
 begin {
-  if ($null -ne $env:RUNNER_DEBUG) { Set-PSDebug -Trace 1 }
+  if ($null -ne $env:RUNNER_DEBUG) { Set-PSDebug -Trace 2 }
   if ($null -eq $env:CI) { throw }
   $ErrorActionPreference = 'Stop'
 
-  $Destination = "${env:ACTION_WORKSPACE}/google-cng"
+  New-Item -ItemType Directory -Path $env:RUNNER_TEMP -Name 'google-cng' | Out-Null
   $GoogleRepository = "GoogleCloudPlatform/kms-integrations"
-
-  if (!(Test-Location $env:ACTION_WORKSPACE)) {
-    throw "Action workspace does not exist. Ensure Setup-Action is run first."
-  }
 }
 
 process {
-  New-Item -ItemType Directory -Name $Destination
-
-  Push-Location -Stack WindowsSigning Destination
-  & gh release download $env:CNG_VERSION --repository $GoogleRepository --pattern "*amd64.zip"
+  Push-Location -Stack WindowsSigning "${env:RUNNER_TEMP}/google-cng"
+  & gh release download $env:CNG_VERSION --repo $GoogleRepository --pattern "*amd64.zip"
   Expand-Archive -Path *.zip
   $SignaturePath = Get-ChildItem *.sig -Recurse
   $MsiPath = Get-ChildItem *.msi -Recurse
@@ -39,5 +33,5 @@ process {
 
   Pop-Location -Stack WindowsSigning
 
-  "google-cng-path=${Destination}" >> $env:GITHUB_OUTPUT
+  "google-cng-path=${env:RUNNER_TEMP}/google-cng" >> $env:GITHUB_OUTPUT
 }
---
+++ .github/actions/publish-obs/setup-bouf/Setup-Bouf.ps1
@@ -0,0 +1,97 @@
+#Requires -Version 7.3
+
+[CmdletBinding(PositionalBinding=$false)]
+param()
+
+begin {
+  if ($null -eq $env:CI) { throw }
+  if ($null -ne $env:RUNNER_DEBUG) { Set-PSDebug -Trace 2 }
+
+  $ErrorActionPreference = 'Stop'
+
+  $BoufUrl = "${env:BOUF_URL}/v${env:BOUF_VERSION}/bouf-windows-v${env:BOUF_VERSION}.zip"
+  $BoufNsisUrl = "${env:BOUF_URL}/v${env:BOUF_VERSION}/bouf-nsis-v${env:BOUF_VERSION}.zip"
+
+  $BoufUrlRegex = '^https:\/\/.+\/v[0-9\.]+\/bouf-windows-v[0-9\.]+\.zip$'
+  $BoufNsisUrlRegex = '^https:\/\/.+\/v[0-9\.]+\/bouf-nsis-v[0-9\.]+\.zip$'
+
+
+  if (!($BoufUrl -match $BoufUrlRegex)) {
+    Write-Output "::error:Invalid BOUF download url: '${BoufUrl}'."
+    throw
+  }
+
+  if (!($BoufNsisUrl -match $BoufNsisUrlRegex)) {
+    Write-Output "::error:Invalid BOUF download url: '${BoufNsisUrl}'."
+    throw
+  }
+
+  $OutputPath = "${env:RUNNER_TEMP}/bouf-windows-v${env:BOUF_VERSION}.zip"
+  $NsisOutputPath = "${env:RUNNER_TEMP}/bouf-nsis-v${env:BOUF_VERSION}.zip"
+}
+
+process {
+  Write-Output '::group::Download BOUF'
+  $WebRequestArguments = @{
+    Uri = $BoufUrl
+    OutFile = $OutputPath
+    SkipHttpErrorCheck = $true
+  }
+
+  $Result = Invoke-WebRequest @WebRequestArguments
+
+  if ($null -ne $Result -and $Result.StatusCode -ne 200) {
+    Write-Output "::error::Unable to download BOUF from '${BoufUrl}'."
+    throw
+  }
+
+  $Checksum = (Get-FileHash -Path $OutputPath -Algorithm SHA256).Hash.ToLower()
+  $File = $OutputPath | Get-Item
+
+  if ($Checksum -ne $env:BOUF_CHECKSUM) {
+    Write-Output "::error::$($File.Name) checksum mismatch: ${Checksum} (expected: ${env:BOUF_CHECKSUM})."
+    throw
+  }
+  Write-Output '::endgroup::'
+
+  Write-Output '::group::Download BOUF NSIS Components'
+  $WebRequestArguments = @{
+    Uri = $BoufNsisUrl
+    OutFile = $NsisOutputPath
+    SkipHttpErrorCheck = $true
+  }
+
+  $Result = Invoke-WebRequest @WebRequestArguments
+
+  if ($null -ne $Result -and $Result.StatusCode -ne 200) {
+    Write-Output "::error::Unable to download BOUF from '${BoufNsisUrl}'."
+    throw
+  }
+
+  $Checksum = (Get-FileHash -Path $NsisOutputPath -Algorithm SHA256).Hash.ToLower()
+  $File = $NsisOutputPath | Get-Item
+
+  if ($Checksum -ne $env:BOUF_NSIS_CHECKSUM) {
+    Write-Output "::error::$($File.Name) checksum mismatch: ${Checksum} (expected: ${env:BOUF_NSIS_CHECKSUM})."
+    throw
+  }
+  Write-Output '::endgroup::'
+
+  Write-Output '::group::Extract BOUF'
+  $ExpandArguments = @{
+    Path = $OutputPath
+    DestinationPath = "${env:RUNNER_TEMP}/bouf"
+  }
+
+  Expand-Archive @ExpandArguments
+
+  $ExpandArguments = @{
+    Path = $NsisOutputPath
+    DestinationPath = "${env:RUNNER_TEMP}/bouf/nsis"
+  }
+
+  Expand-Archive @ExpandArguments
+  Write-Output '::endgroup::'
+
+  "bouf-location=${env::RUNNER_TEMP}/bouf" >> $env:GITHUB_OUTPUT
+}
