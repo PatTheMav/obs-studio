906ec0f064efad07e156e736b8c65cc54571761a
PatTheMav <PatTheMav@users.noreply.github.com>
2026-07-05 02:02:16 +0200

    CI: Add validate-json-schema action

---
+++ .github/actions/lint-obs/validate-json-schema/run-validator.bash
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2154,SC1091
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+: "${CI:?}"
+if [[ -n "${RUNNER_DEBUG:-}" ]]; then set -x; fi
+
+shopt -s extglob
+
+run-validator() {
+  local -a json_files=()
+
+  local output
+  if ! output="$(jq --raw-output '.[]' <<< "${JSON_FILES}")"; then
+    output="$(compgen -G "${JSON_FILES}")"
+  fi
+
+  while read -r file; do
+    full_path="$(realpath "${file}")"
+    json_files+=("${full_path}")
+  done <<< "${output}"
+
+  if (( ! ${#json_files[@]} )); then
+    echo '::error::No json files found to validate.'
+    return 1
+  fi
+
+  source "${RUNNER_TEMP}/compatibility-validator-venv/bin/activate"
+  pushd "${RUNNER_TEMP}" > /dev/null
+  declare -x PYTHONUNBUFFERED=1
+  python3 -u \
+    "${GITHUB_ACTION_PATH}/check-jsonschema.py" \
+    --loglevel INFO \
+    "${json_files[@]}"
+  popd > /dev/null
+
+  deactivate
+}
+
+run-validator
