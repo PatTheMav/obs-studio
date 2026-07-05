c463ff42b8077990bf487d9feea1b11642b11d06
PatTheMav <PatTheMav@users.noreply.github.com>
2026-07-05 02:07:36 +0200

    CI: Add "validate-flatpak-manifest" action
    The action is updated to follow new best practices for composite actions
    within the project:
    
    * Larger script snippets (line number > 5) are factored out into
      separate files.
    * Bash script is linted by ShellCheck.
    * Update used arguments to use simpler words.
    * Arguments use "dash-case" as is common for GitHub actions.
    
    Also updates all workflows and actions that invoked the action to use
    the new arguments.

---
+++ .github/actions/lint-obs/validate-flatpak-manifest/run-validator.bash
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2154
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+: "${CI:?}"
+if [[ -n "${RUNNER_DEBUG:-}" ]]; then set -x; fi
+
+run-validator() {
+  if [[ "${FAIL_MODE:-never}" == 'never' ]]; then set +e; fi
+
+  if [[ ! -r "${MANIFEST_FILE}" ]]; then
+    echo "::error:: Manifest file ${MANIFEST_FILE} not found."
+    return 1
+  fi
+
+  declare -x PYTHONUNBUFFERED=1
+  python3 -u \
+    "${PWD}/build-aux/format-manifest.py" \
+    "${MANIFEST_FILE}" \
+    --check \
+    --loglevel INFO
+}
