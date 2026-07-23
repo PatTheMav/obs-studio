50ee39606bd197c92411e1608b253ed9c3879561
PatTheMav <PatTheMav@users.noreply.github.com>
2026-07-23 23:05:29 +0200

    CI: Add new check-code-format action
    This action unifies the original split actions for each linter and
    uses the updated linter/formatter scripts in build-aux under the hood.
    
    As those scripts are available to a wider range of platforms, the action
    itself can now be used on Windows, macOS, and Linux runners.

---
+++ .github/actions/lint-obs/check-code-format/Setup-Windows.ps1
@@ -0,0 +1,43 @@
+#Requires -Version 7.3
+
+[CmdletBinding(PositionalBinding=$false)]
+param()
+
+begin {
+  if ($null -eq $env:CI) { throw }
+  if ($null -ne $env:RUNNER_DEBUG) { Set-PSDebug -Trace 1 }
+
+  $ErrorActionPreference = 'Stop'
+}
+
+process {
+  $WingetArguments = @(
+    "--silent"
+    "--accept-package-agreements"
+    "--accept-source-agreements"
+    "--disable-interactivity"
+    "--exact"
+  )
+
+  case ( $env:LINTER_COMMAND ) {
+    clang-format {
+      break
+    }
+    gersemi {
+      winget install @WingetArguments BlankSpruce.Gersemi
+      break
+    }
+    zizmor {
+      winget install @WingetArguments zizmor.zizmor
+      break
+    }
+    { ( $_ -eq "swift-format" ) -or ( $_ -eq "xmllint" ) } {
+      Write-Output "::error::The linter '${env:LINTER_COMMAND}' is supported on macOS and Linux only."
+      throw
+    }
+    default {
+      Write-Output "::error::Unsupported linter '${env:LINTER_COMMAND}' provided."
+      throw
+    }
+  }
+}
