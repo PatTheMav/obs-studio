fc92d597d35e68d391611487cd9ec7ce5840b65d
PatTheMav <PatTheMav@users.noreply.github.com>
2026-07-23 22:51:46 +0200

    CI: Add check-version-tag action
    The check-version-tag action checks whether the name of the provided git
    ref represents a semantic version string. If possible, the version
    segments are extracted from a valid version string and the action can
    also optionally fail if no valid version string is detected.
    
    The action is authored with new best practices for composite actions
    within the project:
    
    * Larger script snippets (line number > 5) are factored out into
      separate files.
    * Every script snippet uses Bash exclusively, with all scripts linted
      by ShellCheck.
    * Only Zsh scripts (launched by Bash) are allowed as an alternative
      (due to Powershell truncating debug output and potentially leaking
      partial secrets).
    * Arguments use "dash-case" as is common for GitHub actions.

---
+++ .github/actions/check-version-tag/check-version-tag.bash
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2154
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+: "${CI:?}"
+if [[ -n "${RUNNER_DEBUG:-}" ]]; then set -x; fi
+
+check-version-tag() {
+  local version_regex='^(([0-9]+)\.([0-9]+)\.([0-9]+))(-(rc|beta)([0-9]+))?$'
+
+  if [[ "${GIT_REF}" =~ ${version_regex} ]]; then
+    local -i num_matches="${#BASH_REMATCH[@]}"
+
+    if (( num_matches >= 4 )); then
+      echo "Semantic version detected for ${GIT_REF}."
+      {
+        echo "version=${BASH_REMATCH[0]}"
+        echo "major=${BASH_REMATCH[2]}"
+        echo "minor=${BASH_REMATCH[3]}"
+        echo "patch=${BASH_REMATCH[4]}"
+        echo "is-valid-semver=true"
+      } >> "${GITHUB_OUTPUT}"
+      return 0
+    fi
+
+    if (( num_matches == 8 )); then
+      echo "Semantic pre-release version detected for ${GIT_REF}."
+      {
+        echo "prerelease=${BASH_REMATCH[-2]}"
+        echo "number=${BASH_REMATCH[-1]}"
+        echo "is-valid-semver=true"
+      } >> "${GITHUB_OUTPUT}"
+      return 0
+    fi
+  fi
+
+  echo "is-valid-semver=false" >> "${GITHUB_OUTPUT}"
+  local error_message="No semantic version detected for ${GIT_REF}."
+  if [[ "${FAIL_ON_MISMATCH:-false}" == 'true' ]]; then
+    echo "::error::${error_message}"
+    return 1
+  else
+    echo "${error_message}"
+  fi
+}
+
+check-version-tag
